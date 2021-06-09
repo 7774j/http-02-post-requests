9ca2cbf304c482bb434129d35d5966171175fa5f
7774j <80786885+7774j@users.noreply.github.com>
2021-06-09 12:02:49 -0500

    work due June 7th

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,9 +1,11 @@
-import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 
 export class AuthInterceptorService implements HttpInterceptor{
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     //if (req.url)  that is if you don't want to send to a certain url
-    console.log('Request is on its way!');
-    return next.handle(req);
+    const modifiedRequest = req.clone({
+      headers: req.headers.append('Auth', 'xyz')
+    });
+    return next.handle(modifiedRequest)
   }
 }
